e759cd17bb16699e637ad1347ea8e1d49bd52a00
nPhil01 <niklas.philippi@web.de>
2017-01-16 22:44:12 +0100

    Style änderungen, einbinden von leaflet, anfänge eines Dateimanagemeents

--- app_client/controllers/current.controller.js
+++ app_client/controllers/current.controller.js
@@ -5,7 +5,46 @@
         .controller('currentCtrl', currentCtrl);
 
     currentCtrl.$inject = ['$location', 'meanData', 'userService'];
-    function currentCtrl($location, meanData, userService) {
+    function currentCtrl($location, meanData, userService, $scope) {
         console.log("current Controller is running!!!");
+
+        //*****************************************************************************************************
+        //*****************************************************************************************************
+        //*****************************************************************************************************
+        // Treeview
+
+        tree = new webix.ui({
+            container:"treebox",
+            view:"tree",
+            data: [
+                { id:"branch1", value:"The Shawshank Redemption", data:[
+                    { id:"part1", value:"Part 1" },
+                    { id:"part2", value:"Part 2" }
+                ]}
+            ]
+        });
+
+        //'node' variable will contain an object of the related tree node
+        var node = tree.getItem('branch1');
+
+        //you can access data members directly
+        var value = node.value; // ->"The Shawshank Redemption"
+
+        //*****************************************************************************************************
+        //*****************************************************************************************************
+        //*****************************************************************************************************
+
+        /* var treeview;
+
+        $scope.buttonCodeToggle = function(){
+            angular.element( document.querySelector('#code')).addClass('active');
+            angular.element( document.querySelector('#otherdata')).removeClass('active');
+            treeview = false;
+        };
+        $scope.buttonTreeToggle = function(){
+            //angular.element( document.querySelector('#code')).removeClass('active');
+            // angular.element( document.querySelector('#otherdata')).addClass('active');
+            treeview = true;
+        }; */
     }
 })();
