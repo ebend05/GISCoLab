81d7d07f67bb8b5699a2e08d9e805913526ded56
Elisa B <ebender727@gmail.com>
2017-02-03 17:18:31 +0100

    fixed project dropdown

--- app_client/controllers/projects.controller.js
+++ app_client/controllers/projects.controller.js
@@ -4,8 +4,25 @@
         .module('giscolab')
         .controller('projectsCtrl', projectsCtrl);
 
-    projectsCtrl.$inject = ['$location', 'meanData', 'userService'];
-    function projectsCtrl($location, meanData, userService) {
+    projectsCtrl.$inject = ['$location', 'meanData', 'userService', '$scope', '$rootScope', 'projectService'];
+    function projectsCtrl($location, meanData, userService, $scope, $rootScope, projectService) {
         console.log("projects Controller is running!!!");
+
+        var vm = this;
+
+        vm.user = {};
+
+        meanData.getProfile()
+            .success(function(data) {
+                vm.user = data;
+            })
+            .error(function (e) {
+                console.log(e);
+            });
+
+        $scope.myFunction = function (id) {
+            projectService.setID(id);
+            $location.path('/projectActive');
+        }
     }
 })();
