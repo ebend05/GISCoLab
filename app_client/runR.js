f68b337bad5b226500baa51a4aac37592afb4f02
Timmimim <timm.kuehnel@gmail.com>
2017-02-05 15:19:19 +0100

    minimal changes to current.view, still not working, plus minor changes to runR.js

--- app_client/runR.js
+++ app_client/runR.js
@@ -7,11 +7,13 @@ $('#runCode').on('click', function () {
 
 function runR ()
 {
-	//var uniqueKey = document.getElementById('uniqueKey').value;
+	var uniqueKey = document.getElementById('uniqueKey').value;
 
 	var fName = document.getElementById('fileName').value;
 	var pkg = document.getElementById('packages').value;
 	var code = document.getElementById('codearea').value;
+
+	var output = [];
 
 	var data = {
 		fName: fName,
@@ -26,15 +28,18 @@ function runR ()
 		headers: {
 			Authorization: 'Bearer ' + localStorage['mean-token']
 		},
-		url: "/api/runRCode",
-		timeout: 10000,
+		url: "/api/runRCode/"+ uniqueKey,
+		timeout: 10000000,
 		success: function(data, textStatus){
 			console.log("successfully saved : " + textStatus);
 			//console.log(data);
 			//TODO: Trigger Data Tree Update
+
+			alert("<bold>Berechnung von "+ fname+" abgeschlossen!</bold> \n\n Die resultierenden TMS Layer liegen im entsprechenden Projektordner bereit.");
 		},
 		error: function(xhr, textStatus, errorThrown){
 			console.log("saving failed");
+			alert("Something went wrong while running "+fname+".");
 		}
 	})
 }
